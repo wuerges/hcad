61c5ef38b2dee10f0b168aff447031721fb46d8a
Emilio Wuerges <wuerges@gmail.com>
2019-11-05 19:10:14 -0300

    instnalling quickcheck

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Geometry
+import Test.QuickCheck
+
+instance Arbitrary Point where
+    arbitrary = do
+        Positive x <- arbitrary
+        Positive y <- arbitrary
+        Positive z <- arbitrary
+        return $ P [x, y, z]
+
+
+instance Arbitrary Rectangle where
+    arbitrary = do
+        Point p1 <- arbitrary        
+        Point p2 <- arbitrary        
+        return $ R (minP p1 p2) (maxP p1 p2)
+    
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
