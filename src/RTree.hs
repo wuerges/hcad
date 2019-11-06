f9cee4d2415649bd322cbd0b22aa6f29f0a8f4cc
Emilio Wuerges <wuerges@gmail.com>
2019-11-06 18:51:59 -0300

    split not ok when all rectangles are equal

--- src/RTree.hs
+++ src/RTree.hs
@@ -2,8 +2,9 @@ module RTree
     ( empty
     , singleton
     , insert
-    , RTree
+    , RTree (Empty, Leaf, Child)
     , RTree.elem
+    , splitList
     ) where
 
 import Geometry
@@ -14,7 +15,8 @@ maximumSize = 8
 -- minimumSize = maximumSize `div` 2
 
 
-data RTree a = Empty | Leaf Rectangle a | Child Rectangle [(Rectangle, RTree a)]
+data RTree a = Empty | Leaf Rectangle a | Child Rectangle [RTree a]
+        deriving Show
 
 
 empty :: RTree a
@@ -24,21 +26,30 @@ singleton :: Rectangle -> a -> RTree a
 singleton = Leaf
 
 
-splitList :: [(Rectangle, a)] -> 
-    Either [(Rectangle, a)] (Rectangle, [(Rectangle, a)], Rectangle, [(Rectangle, a)])
+splitList :: [RTree a] -> 
+    Either [RTree a] (RTree a, RTree a)
 splitList l = 
     if length l < maximumSize then Left l else Right $ work l
     where
-        (left_h, right_h):rem = sortOn (\(x,y) -> -(areaR $ mbr x y)) [(a, b) | a <- map fst l, b <- map fst l]
+        (left_h, right_h):rem = sortOn (\(x,y) -> -(areaR $ mbr x y)) [(a, b) | a <- map mbr' l, b <- map mbr' l]
 
-        work :: [(Rectangle, b)] -> (Rectangle, [(Rectangle, b)], Rectangle, [(Rectangle, b)])
-        work = foldl f (left_h, [], right_h, [])
+        work :: [RTree b] -> (RTree b, RTree b)
+        work = foldl f (Child left_h [], Child right_h [])
         
-        f :: (Rectangle, [(Rectangle, b)], Rectangle, [(Rectangle, b)]) -> (Rectangle, b) -> (Rectangle, [(Rectangle, b)], Rectangle, [(Rectangle, b)])
-        f (rect_a, a, rect_b, b) (r, v) = 
-            if mbr r rect_a < mbr r rect_b 
-                then (mbr r rect_a, (r,v):a,       rect_b,       b)
-                else (      rect_a,       a, mbr r rect_b, (r,v):b)
+        f :: (RTree b, RTree b) -> RTree b -> (RTree b, RTree b)
+        f (a@(Child ra ca), b@(Child rb cb)) t = 
+            let rt = mbr' t in
+            if mbr ra rt < mbr rb rt
+                then (Child (mbr ra rt) (t:ca), b)
+                else (a, Child (mbr rb rt) (t:cb))
+        -- f (a@(Child ra ca), b@(Child rb cb)) t@(Leaf rt ct) = 
+        --     if mbr ra rt < mbr rb rt
+        --         then (Child (mbr ra rt) (t:ca), b)
+        --         else (a, Child (mbr rb rt) (t:cb))
+        --         f (rect_a, a, rect_b, b) (r, v) = 
+--             if mbr r rect_a < mbr r rect_b 
+--                 then (mbr r rect_a, (r,v):a,       rect_b,       b)
+--                 else (      rect_a,       a, mbr r rect_b, (r,v):b)
 
 mbr = minimumBoundRect
 
@@ -46,6 +57,7 @@ mbr1 :: [Rectangle] -> Rectangle
 mbr1 l = foldl1 mbr l
 
 
+mbr' :: RTree a -> Rectangle
 mbr' Empty = error "empty has to mbr"
 mbr' (Leaf bb _) = bb
 mbr' (Child bb _) = bb
@@ -59,22 +71,19 @@ insertNodeP r v (Leaf bb l) = Right (Leaf r v, Leaf bb l)
 insertNodeP r v (Child bb l) =
     case splitList l_ of
         Left single -> Left $ Child (mbr bb mbr_) single
-        Right (mbr_a, a, mbr_b, b) -> Right (Child mbr_a a, Child mbr_b b)
+        Right (a, b) -> Right (a, b)
     where 
-        (rh, nh) : hs = sortOn (\x -> mbr r (fst x)) l
-        (mbr_, l_) = case insertNodeP r v nh of
-            Left no_split -> (mbr' no_split, (mbr' no_split, no_split):hs)
-            Right (one, two) -> (mbr (mbr' one) (mbr' two), (mbr' one, one):(mbr' two, two):hs)
+        h : hs = sortOn (\x -> areaR $ mbr r $ mbr' x) l
+        (mbr_, l_) = case insertNodeP r v h of
+            Left no_split -> (mbr (mbr' no_split) bb, no_split:hs)
+            Right (one, two) -> (mbr1 [mbr' one, mbr' two, bb], one:two:hs)
 
 
 insert :: Rectangle -> a -> RTree a -> RTree a
 insert r v t = 
     case insertNodeP r v t of
         Left single -> single
-        Right (a, b) -> Child (mbr mbr_a mbr_b) [(mbr_a, a), (mbr_b, b)]
-            where 
-                mbr_a = mbr' a
-                mbr_b = mbr' b
+        Right (a, b) -> Child (mbr (mbr' a) (mbr' b)) [a, b]
 
 
 
@@ -88,5 +97,5 @@ elem r (Leaf bb _) =
 elem r (Child bb l) = 
     case intersection r bb of
         Nothing -> False
-        Just _ -> any (RTree.elem r) (map snd l)
+        Just _ -> any (RTree.elem r) l
         
