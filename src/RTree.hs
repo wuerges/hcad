a298448d829a33ea3dd544ec59008fad1e2057ee
Emilio Wuerges <wuerges@gmail.com>
2019-11-06 15:10:49 -0300

    compiled

--- src/RTree.hs
+++ src/RTree.hs
@@ -11,31 +11,18 @@ maximumSize = 8
 -- minimumSize = maximumSize `div` 2
 
 
-data Node a = Empty | Leaf Rectangle [(Rectangle, a)] | Child Rectangle [(Rectangle, Node a)]
-data BNode a = BNode Rectangle (Node a)
-
-data RTree a = Node (Node a)
+data RTree a = Empty | Leaf Rectangle a | Child Rectangle [(Rectangle, RTree a)]
 
 
-makeRTree :: Node a
+makeRTree :: RTree a
 makeRTree = Empty
 
 
--- splitList :: [(Rectangle, a)] -> ([(Rectangle, a)],[(Rectangle, a)])
 splitList :: [(Rectangle, a)] -> 
     Either [(Rectangle, a)] (Rectangle, [(Rectangle, a)], Rectangle, [(Rectangle, a)])
 splitList = undefined
 
 mbr = minimumBoundRect
-
--- checkSplit :: [(Rectangle, a)] -> 
--- checkSplit :: Rectangle -> Node a -> [(Rectangle, Node a)]
--- checkSplit = undefined
--- checkSplit l
---     | length l < maximumSize = l
---     | otherwise              = Child $ splitList l
-
-
 
 mbr1 :: [Rectangle] -> Rectangle
 mbr1 l = foldl1 mbr l
@@ -45,26 +32,29 @@ mbr' Empty = error "empty has to mbr"
 mbr' (Leaf bb _) = bb
 mbr' (Child bb _) = bb
 
-insertNode :: Rectangle -> a -> Node a -> Either (Node a) (Node a, Node a)
+insertNodeP :: Rectangle -> a -> RTree a -> Either (RTree a) (RTree a, RTree a)
 
-insertNode r v Empty = Left $ Leaf r [(r,v)]
+insertNodeP r v Empty = Left $ Leaf r v
 
-insertNode r v (Leaf bb l) = 
-    case splitList l' of
-        Left single -> Left $ Leaf (mbr bb r) single
-        Right (mbr_a, a, mbr_b, b) -> Right (Leaf mbr_a a, Leaf mbr_b b)
-    where 
-        l' = (r,v):l
+insertNodeP r v (Leaf bb l) = Right (Leaf r v, Leaf bb l)
 
-
-insertNode r v (Child bb l) =
+insertNodeP r v (Child bb l) =
     case splitList l_ of
         Left single -> Left $ Child (mbr bb mbr_) single
         Right (mbr_a, a, mbr_b, b) -> Right (Child mbr_a a, Child mbr_b b)
     where 
         (rh, nh) : hs = sortOn (\x -> mbr r (fst x)) l
-        (mbr_, l_) = case insertNode r v nh of
+        (mbr_, l_) = case insertNodeP r v nh of
             Left no_split -> (mbr' no_split, (mbr' no_split, no_split):hs)
             Right (one, two) -> (mbr (mbr' one) (mbr' two), (mbr' one, one):(mbr' two, two):hs)
 
+
+insertNode :: Rectangle -> a -> RTree a -> RTree a
+insertNode r v t = 
+    case insertNodeP r v t of
+        Left single -> single
+        Right (a, b) -> Child (mbr mbr_a mbr_b) [(mbr_a, a), (mbr_b, b)]
+            where 
+                mbr_a = mbr' a
+                mbr_b = mbr' b
 
