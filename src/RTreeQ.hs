ff0da621460ee3229bf498bf30b6d7983bedeef6
Emilio Wuerges <wuerges@gmail.com>
2019-11-17 14:26:20 -0300

    RTreeQ deque ok

--- src/RTreeQ.hs
+++ src/RTreeQ.hs
@@ -6,11 +6,7 @@ import Control.Monad.State.Lazy
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-
-
-
 data MBRSet a = MBRSet Rectangle (Map Int (RTree a))
-
 
 pop :: MBRSet a -> Maybe (RTree a, MBRSet a)
 pop (MBRSet bb s) = do
@@ -23,20 +19,20 @@ push l (MBRSet bb s) = MBRSet bb (Map.insert area l s)
     where
         area = areaR $ mbr' l `minimumBoundRect` bb
 
+pushMany :: [RTree a] -> MBRSet a -> MBRSet a
+pushMany ts s = foldl (flip push) s ts
 
 deque :: StateT (MBRSet a) Maybe a
 deque = do
-    s <- get
-    case s of
-        [] -> lift Nothing
-        (h:hs) -> case h of 
-            Empty -> do
-                put hs
-                deque
-            Leaf _ v -> do
-                put hs
-                lift $ Just v
-            Child _ ts -> do
-                put $ ts ++ hs
-                deque
-
+    x <- get
+    (h, s') <- lift $ pop x
+    case h of
+        Empty -> do
+            put s'
+            deque
+        Leaf _ v -> do
+            put s'
+            lift $ Just v
+        Child _ ts -> do
+            put $ pushMany ts s'
+            deque
